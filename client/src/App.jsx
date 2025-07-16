fcf06487b3ae4e221771beed60b2995c5d213d49
Khadijah Clark <Khadijahnclark@gmail.com>
2025-07-15 19:58:38 -0500

    installing vite react and tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <h1 className="text-4xl font-bold text-blue-600">
+      Hello Tailwind v4!
+    </h1>
+  );
+}
+
+export default App
